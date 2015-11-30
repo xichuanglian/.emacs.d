cd4932b199ee3dd19b955e7fd34097e83229e29f
Da Wei <xichuanglian@gmail.com>
2015-11-30 14:41:58 +0800

    updated init-company

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -3,8 +3,8 @@
 
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
-(setq company-idle-delay 0.5)
-(setq company-minimum-prefix-length 3)
+(setq company-idle-delay 0.1)
+(setq company-minimum-prefix-length 2)
 (global-set-key (kbd "M-/") 'company-complete)
 
 (provide 'init-company)
