dd4d82cdd1eaaea545cad0bd51f972c6ca2cb678
Da Wei <xichuanglian@gmail.com>
2015-12-02 21:34:39 +0800

    stable packets only

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -1,11 +1,5 @@
 (require 'init-melpa)
 (require-package 'helm)
-(require-package 'helm-company)
 (require 'helm-config)
-(require 'helm-company)
 (helm-mode 1)
-(eval-after-load 'company
-  '(progn
-     (define-key company-mode-map (kbd "M-/") 'helm-company)
-     (define-key company-active-map (kbd "M-/") 'helm-company)))
 (provide 'init-helm)
--- lisp/init-melpa.el
+++ lisp/init-melpa.el
@@ -1,6 +1,6 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+;(add-to-list 'package-archives
+;             '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
