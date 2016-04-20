439b388bd4abb803118dc3ae1ed04041bcc4e605
xichuanglian <xichuanglian@gmail.com>
2016-04-21 00:07:51 +0800

    smartparens default keybinding

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -5,7 +5,6 @@
 (show-smartparens-global-mode t)
 (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
 (add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)
-(sp-use-paredit-bindings)
 
 (provide 'init-smartparens)
 
